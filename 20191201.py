b2caf9044514920675ff1cbdfbad30a3c25c1b2d
Louis-Marie NOGUES <louis-marie.nogues@orange.com>
2019-12-02 10:04:48 +0100

    day one part 2

--- 20191201.py
+++ 20191201.py
@@ -4,40 +4,80 @@ import requests
 def get_input_from_file(file):
     with open(file, "r") as file_object:
         results = file_object.read().splitlines()
-        print(results)
         return results
 
 
-def calculate_day_one(inputs):
+def calculate_day_one_part_one(inputs):
     total_fuel = 0
     for mass in inputs:
         total_fuel = total_fuel + int(int(mass)/3) - 2
     return total_fuel
 
 
-def test_calculate_day_one():
+def calculate_day_two_fuel(mass):
+    return max(0, int(int(mass)/3) - 2)
+
+
+def calculate_day_one_part_two(inputs):
+    total_fuel = 0
+    for mass in inputs:
+        fuel_for_mass = calculate_day_two_fuel(mass)
+        total_fuel = total_fuel + fuel_for_mass
+        while fuel_for_mass > 0:
+            fuel_for_mass = calculate_day_two_fuel(fuel_for_mass)
+            total_fuel = total_fuel + fuel_for_mass
+    return total_fuel
+
+
+def test_calculate_day_one_part_one():
     test_list = list()
+
     test_list.append('12')
-    assert calculate_day_one(test_list) == 2
+    r = calculate_day_one_part_one(test_list)
+    assert r == 2
     test_list.clear()
+
     test_list.append('14')
-    assert calculate_day_one(test_list) == 2
+    r = calculate_day_one_part_one(test_list)
+    assert r == 2
     test_list.clear()
+
     test_list.append('1969')
-    assert calculate_day_one(test_list) == 654
+    r = calculate_day_one_part_one(test_list)
+    assert r == 654
     test_list.clear()
+
     test_list.append('100756')
-    assert calculate_day_one(test_list) == 33583
+    r = calculate_day_one_part_one(test_list)
+    assert r == 33583
 
 
-def main():
-    print("starting")
-    inputs = get_input_from_file('./20191201.txt')
-    day_one_total_mass = calculate_day_one(inputs)
-    print(day_one_total_mass)
+def test_calculate_day_one_part_two():
+    test_list = list()
+
+    test_list.append('14')
+    r = calculate_day_one_part_two(test_list)
+    assert r == 2
+    test_list.clear()
+
+    test_list.append('1969')
+    r = calculate_day_one_part_two(test_list)
+    assert r == 966
+    test_list.clear()
+
+    test_list.append('100756')
+    r = calculate_day_one_part_two(test_list)
+    assert r == 50346
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    test_calculate_day_one()
-    main()
+    print("validating")
+    test_calculate_day_one_part_one()
+    test_calculate_day_one_part_two()
+    print("processing days")
+    inputs = get_input_from_file('./20191201.txt')
+    day_one_total_mass = calculate_day_one_part_one(inputs)
+    print(day_one_total_mass)
+    day_two_total_mass = calculate_day_one_part_two(inputs)
+    print(day_two_total_mass)
