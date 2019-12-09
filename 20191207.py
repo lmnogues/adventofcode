3ed6baeb1a6b185683f81389399c0d5e7a6c3479
Jalkar <jalkaru@gmail.com>
2019-12-09 10:25:46 +0100

    day 7 part 2

--- 20191207.py
+++ 20191207.py
@@ -1,5 +1,14 @@
 from itertools import permutations
 
+class Amplifier:
+    def __init__(self, program):
+        self.program = program.copy()
+        self.output=0
+        self.cursor=0
+        self.iteration=0
+        self.stop=False
+
+
 
 def get_input_from_file(file):
     with open(file, "r") as file_object:
@@ -15,12 +24,9 @@ def convert_input(inputs):
     return results
 
 
-def walk_input(inputs, opCode3_input, phase):
-    i = 0
-    is_first_input = True
-    returned_value = opCode3_input
-    while inputs[i] != 99:
-        instr = f"{inputs[i]:05}"
+def run_pgm(amplifier, phase,input):
+    while amplifier.program[amplifier.cursor] != 99:
+        instr = f"{amplifier.program[amplifier.cursor]:05}"
         opCode = instr[-2:]
         paramCode_param1 = instr[2]
         paramCode_param2 = instr[1]
@@ -28,93 +34,101 @@ def walk_input(inputs, opCode3_input, phase):
 
         if opCode in ['01', '02', '04', '05', '06', '07', '08']:
             if paramCode_param1 == '0':
-                valueA = inputs[inputs[i+1]]
+                valueA = amplifier.program[amplifier.program[amplifier.cursor+1]]
             elif paramCode_param1 == '1':
-                valueA = inputs[i+1]
+                valueA = amplifier.program[amplifier.cursor+1]
 
             if opCode in ['01', '02', '05', '06', '07', '08']:
                 if paramCode_param2 == '0':
-                    valueB = inputs[inputs[i+2]]
+                    valueB = amplifier.program[amplifier.program[amplifier.cursor+2]]
                 elif paramCode_param2 == '1':
-                    valueB = inputs[i+2]
+                    valueB = amplifier.program[amplifier.cursor+2]
 
                 if opCode in []:
                     if paramCode_param3 == '0':
-                        valueC = inputs[inputs[i+3]]
+                        valueC = amplifier.program[amplifier.program[amplifier.cursor+3]]
                     elif paramCode_param3 == '1':
-                        valueC = inputs[i+3]
+                        valueC = amplifier.program[amplifier.cursor+3]
 
         if opCode == '01':
-            inputs[inputs[i+3]] = valueA+valueB
-            i += 4
+            amplifier.program[amplifier.program[amplifier.cursor+3]] = valueA+valueB
+            amplifier.cursor += 4
         elif opCode == '02':
-            inputs[inputs[i+3]] = valueA*valueB
-            i += 4
+            amplifier.program[amplifier.program[amplifier.cursor+3]] = valueA*valueB
+            amplifier.cursor += 4
         elif opCode == '03':
-            if is_first_input:
+            if amplifier.iteration == 0:
                 val = phase
-                is_first_input = False
             else:
-                val = returned_value
-            inputs[inputs[i+1]] = val
-            i += 2
+                val = input
+            amplifier.iteration+=1
+            amplifier.program[amplifier.program[amplifier.cursor+1]] = val
+            amplifier.cursor += 2
         elif opCode == '04':
-            returned_value = valueA
-            i += 2
+            amplifier.output = valueA
+            amplifier.cursor += 2
+            return amplifier
         elif opCode == '05':
             if valueA != 0:
-                i = valueB
+                amplifier.cursor = valueB
             else:
-                i += 3
+                amplifier.cursor += 3
         elif opCode == '06':
             if valueA == 0:
-                i = valueB
+                amplifier.cursor = valueB
             else:
-                i += 3
+                amplifier.cursor += 3
         elif opCode == '07':
             if valueA < valueB:
-                inputs[inputs[i+3]] = 1
+                amplifier.program[amplifier.program[amplifier.cursor+3]] = 1
             else:
-                inputs[inputs[i+3]] = 0
-            i += 4
+                amplifier.program[amplifier.program[amplifier.cursor+3]] = 0
+            amplifier.cursor += 4
         elif opCode == '08':
             if valueA == valueB:
-                inputs[inputs[i+3]] = 1
+                amplifier.program[amplifier.program[amplifier.cursor+3]] = 1
             else:
-                inputs[inputs[i+3]] = 0
-            i += 4
-    return returned_value
+                amplifier.program[amplifier.program[amplifier.cursor+3]] = 0
+            amplifier.cursor += 4
+    amplifier.stop = True
+    return amplifier
 
-def calculate_all_possible_output(inputs,provided_input):
-    phase_results={}
-    for phase in permutations(range(5),5):
-        input_code=provided_input
-        for step in phase:
-            input_code=walk_input(inputs.copy(),input_code,step)          
-        phase_results[phase] = input_code  
+
+def calculate_all_possible_output(inputs, provided_input):
+    phase_results = {}
+    phases = permutations(range(5, 10), 5)
+    for phase in phases:
+        amps=[Amplifier(inputs) for step in range(5)]
+        
+        while not amps[4].stop:
+            for i in range(5):
+                amps[i]=run_pgm(amps[i],phase[i],amps[i-1].output)
+                phase_results[phase]=amps[i].output
     return phase_results
 
-def find_max_output(inputs,provided_input):
-    phase_results =calculate_all_possible_output(inputs,provided_input)
+def find_max_output(inputs, provided_input):
+    phase_results = calculate_all_possible_output(inputs,provided_input)
     max_value = max([res for res in phase_results.values()])
     return max_value
+
 
 def input_as_text(inputs):
     return ','.join((str(x) for x in inputs))
 
 
 def test_walk_input():
-    inputs=[3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
-    out = find_max_output(inputs,0)
-    assert out == 43210
-    
-    inputs=[3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
-    out = find_max_output(inputs,0)
-    assert out == 54321
+    inputs = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    out = find_max_output(inputs, 0)
+    assert out == 139629729
 
-    inputs=[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+    inputs=[3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
     out = find_max_output(inputs,0)
-    assert out == 65210
+    assert out == 18216
+
+    # inputs=[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+    # out = find_max_output(inputs,0)
+    # assert out == 65210
+
 
 if __name__ == "__main__":
     # execute only if run as a script
