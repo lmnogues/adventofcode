2e96c1771e4f965bb5e72d662899035a9d6e2cfb
Jalkar <jalkaru@gmail.com>
2019-12-10 15:04:46 +0100

    day 10 part 2

--- 20191210.py
+++ 20191210.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def get_input_from_file(file):
@@ -52,38 +53,31 @@ def prepare_constelation():
 def extract_coordinates(constelation):
     ii = np.where(constelation == '#')
     coordinates = tuple(zip(ii[1], ii[0]))
-    print(coordinates)
+    # print(coordinates)
     return coordinates
 
 
-if __name__ == "__main__":
-    constelation = prepare_constelation()
-    coordinates = extract_coordinates(constelation)
+def determine_monitoring_position(coordinates):
     all_result = dict()
-    x = [5, 8]
-    y = [1, 7]
-    z = [9, 9]
-
     for A in coordinates:
         eq_dict = dict()
         for B in coordinates:
-            if A != B or A == B:
-                eq = get_equation(A, B)
-                distance = (A[0] - B[0]) + (A[1] - B[1])
-                dir = 1
-                if distance < 0:
-                    dir = 0
-                elif distance > 0:
-                    dir = 2
+            eq = get_equation(A, B)
+            distance = (A[0] - B[0]) + (A[1] - B[1])
+            dir = 1
+            if distance < 0:
+                dir = 0
+            elif distance > 0:
+                dir = 2
 
-                list_coord = eq_dict.get(eq)
-                key = (eq, dir)
-                if list_coord == None:
-                    list_coord = list()
-                list_coord.append(B)
-                eq_dict[key] = list_coord
+            list_coord = eq_dict.get(eq)
+            key = (eq, dir)
+            if list_coord == None:
+                list_coord = list()
+            list_coord.append(B)
+            eq_dict[key] = list_coord
         all_result[A] = eq_dict
-        #print("Coordinates {0} - result : {1}".format(A, len(eq_dict.keys())))
+        # print("Coordinates {0} - result : {1}".format(A, len(eq_dict.keys())))
 
     nb_visible = 0
     max_coord = None
@@ -94,3 +88,37 @@ if __name__ == "__main__":
             max_coord = coord
 
     print("MAX Coordinates {0} - result : {1}".format(max_coord, nb_visible))
+    return all_result[max_coord], max_coord
+
+
+def calculate_angles(station, asteroids):
+
+    asteroid_angles = dict()
+    for asteroid in asteroids:
+        x = asteroid[0]
+        y = asteroid[1]
+        cx = station[0]
+        cy = station[1]
+        calc = math.atan2((x - cx), (y - cy))
+        asteroid_angles[asteroid] = calc
+    s = [(k, asteroid_angles[k]) for k in sorted(asteroid_angles, key=asteroid_angles.get, reverse=True)]
+    return s
+
+
+if __name__ == "__main__":
+    constelation = prepare_constelation()
+    coordinates = extract_coordinates(constelation)
+    equation_planets, max_coord = determine_monitoring_position(coordinates)
+    print(max_coord)
+    asteroid_to_destroy = list()
+    for equation in equation_planets:
+        for coord in equation_planets[equation]:
+            if coord != max_coord and coord not in asteroid_to_destroy:
+                asteroid_to_destroy.append(coord)
+    # print(asteroid_to_destroy)
+    # print(len(asteroid_to_destroy))
+    asteroid_to_destroy_sorted = calculate_angles(max_coord, asteroid_to_destroy)
+    count = 1
+    for aste in asteroid_to_destroy_sorted:
+        print(count, aste)
+        count += 1
