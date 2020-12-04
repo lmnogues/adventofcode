4792651a3054dce04ae86a666314d773b2f7ee5f
Jalkar <Jalkaru@gmail.com>
2020-12-04 09:48:55 +0100

    cleanup

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,5 +1,6 @@
 import numpy
 import common
+import matplotlib.pyplot as plt
 
 def transform_terrain_to_array(inputs):
     return numpy.vstack([numpy.array(list(line)) for line in inputs])
@@ -10,7 +11,7 @@ def slide_the_terrain(terrain,movement,start_pos):
     current_pos=start_pos
     tree_met=0
     while current_pos[0]< y_size:
-        # print(f"current position is {current_pos} - terrain value is [{terrain[current_pos]}] - tree met so far : {tree_met}")
+        #print(f"current position is {current_pos} - terrain value is [{terrain[current_pos]}] - tree met so far : {tree_met}")
         if terrain[current_pos]=="#":
             tree_met+=1
         y_pos=current_pos[0]+movement[0]
@@ -20,7 +21,7 @@ def slide_the_terrain(terrain,movement,start_pos):
         current_pos=(y_pos,x_pos)
             
     return tree_met
-
+    
 def test():
     inputs=["..##.......",
             "#...#...#..",
@@ -46,9 +47,6 @@ def test():
 
 def main():
     inputs=common.get_input_from_file("day3.txt")
-    
-    lines = [x.strip() for x in inputs]
-
     terrain=transform_terrain_to_array(inputs)
     starting_pos=(0,0)
 
